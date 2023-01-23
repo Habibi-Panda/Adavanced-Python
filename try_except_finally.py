be662777d39c9d53e6b084209646e6476490a111
Habibi-Panda <syit340pandeymadhusudan@gmail.com>
2023-01-23 16:15:44 +0530

    some changes done in try

--- try_except_finally.py
+++ try_except_finally.py
@@ -5,6 +5,10 @@ try:
 
 except Exception as e:
     print(e)
-
+    exit()
+#yaha kuch bhi ho jaye par finally hamesha excute hoga chahe
+# try run ho ya exception aaye finally excute hoga hi
 finally:
     print("We were done")
+
+print("Thanks for using the program")
