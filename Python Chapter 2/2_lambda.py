e6a5dafb0ed87ae751ad19f396eda9446009fcc8
Habibi-Panda <syit340pandeymadhusudan@gmail.com>
2023-01-24 23:08:46 +0530

    Python Chapter 2 - lambda, join and VirtualEnv file created

---
+++ Python Chapter 2/2_lambda.py
@@ -0,0 +1,10 @@
+# def func(x):
+#     return x*x
+
+func = lambda x : x*x
+sum = lambda a,b,c : a + b + c
+cube = lambda y : y*y*y
+
+print(func(5))     #25
+print(sum(5,4,1))  #10
+print(cube(5))     #125
---
+++ Python Chapter 2/3_join.py
@@ -0,0 +1,4 @@
+a = ["Madhusudan", "Panda"]
+
+j = " and ".join(a)
+print(f"{j} both are different personalality.")
